593b66ddddcfc211b3d486cd9bc7d0c112f5ae67
Ruben Benito Martinez <ruben2cc@gmail.com>
2026-06-08 09:06:55 -0600

    Primer commit

---
+++ src/main/java/sample/multibranch/MultibranchApplication.java
@@ -0,0 +1,13 @@
+package sample.multibranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchApplication.class, args);
+	}
+
+}
